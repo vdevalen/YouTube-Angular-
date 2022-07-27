eefbd161d0fe346f65fe04960b2246770a91b0be
vdevalen <vpinzon@sistecredito.com>
2022-07-27 13:39:38 -0500

    "comunicacion entre componentes"

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  public image:string= "https://www.buenamusica.com/media/fotos/discos/b/bad-bunny/bad-bunny_diles.jpg"
+  public image?:string
   public titulo:string= "Diles - Bad Bunny"
+  @Input() dataEntrante:any; //se usa para resibir datos dentro de un componente 
   constructor() { }
 
   
   ngOnInit(): void {
+    this.image= "https://www.buenamusica.com/media/fotos/discos/b/bad-bunny/bad-bunny_diles.jpg"
   }
 
 }
